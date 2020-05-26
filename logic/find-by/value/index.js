2abeabec5c3a81eac2777630325c20bdf6dd04f3
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-26 21:10:55 +0200

    find-by-value completed

--- logic/find-by/value/index.js
+++ logic/find-by/value/index.js
@@ -1,10 +1,29 @@
 // "value" will always be a string, because params always are!
 // consider using loose equality for your comparison
 // (later you will learn about req.body to pass any type)
-
 const value = (pokeArray, value) => {
-
+  const data = pokeArray;
+  const matches = [];
+  let newValue = value;
+  if (value === "null") {
+    newValue = null;
+  }
+  data.map((pokemonObject) => {
+    for (let i in pokemonObject) {
+      const match = pokemonObject[i] == newValue;
+      if (match) {
+        const pokemon = {
+          name: pokemonObject.name,
+          num: pokemonObject.num,
+        };
+        matches.push(pokemon);
+        return;
+      }
+    }
+  });
+  if (matches.length < 1) {
+    return null;
+  }
+  return matches;
 };
-
-
 module.exports = value;
