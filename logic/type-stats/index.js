b48c8302f87141e7dfe30617513f70867bc29a53
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-26 18:30:08 +0200

    type-stats completed

--- logic/type-stats/index.js
+++ logic/type-stats/index.js
@@ -1,5 +1,22 @@
 const typeStats = (pokeArray, type) => {
+  const thisType = pokeArray
+    .map((pokemon) => pokemon.type)
+    .filter((value) => value.includes(`${type}`));
 
+  const typeCount = thisType.length;
+
+  const thisTypeInWeaknesses = pokeArray
+    .map((pokemon) => pokemon.weaknesses)
+    .filter((value) => value.includes(`${type}`));
+
+  const weaknessCount = thisTypeInWeaknesses.length;
+
+  const result = {
+    typeCount: typeCount,
+    typeName: `${type}`,
+    weaknessCount: weaknessCount,
+  };
+  return result;
 };
 
 module.exports = typeStats;
