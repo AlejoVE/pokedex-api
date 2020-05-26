2d5bbd321b2fd28d3b65fe5da559466eda242ecc
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-26 21:29:25 +0200

    all logic completed
    find-by-weakness completed

--- logic/find-by/weakness/index.js
+++ logic/find-by/weakness/index.js
@@ -1,5 +1,16 @@
 const weakness = (pokeArray, type) => {
-
+  const pokemons = [];
+  const data = pokeArray.filter((pokemon) => pokemon.weaknesses.includes(type));
+  if (data.length < 1) {
+    return pokemons;
+  }
+  data.forEach((pokemonData) => {
+    const pokemon = {
+      name: pokemonData.name,
+      num: pokemonData.num,
+    };
+    pokemons.push(pokemon);
+  });
+  return pokemons;
 };
-
 module.exports = weakness;
