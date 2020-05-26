2924ff5981c0af1b1d5feb94157c79983802f0f8
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-26 19:41:19 +0200

    find-by-key-value completed

--- logic/find-by/key-value/index.js
+++ logic/find-by/key-value/index.js
@@ -1,10 +1,24 @@
 // "value" will always be a string, because params always are!
 // consider using loose equality for your comparison
 // (later you will learn about req.body to pass any type)
-
 const keyValue = (pokeArray, key, value) => {
+  const pokemons = [];
+  const valueToString = value.toString();
+  const data = pokeArray.filter(
+    (pokemon) => pokemon[`${key}`] == valueToString
+  );
+  if (data.length < 1) {
+    return null;
+  }
+  data.forEach((pokemonData) => {
+    const pokemon = {
+      num: pokemonData.num,
+      name: pokemonData.name,
+    };
+    pokemons.push(pokemon);
+  });
 
+  return pokemons;
 };
-
 
 module.exports = keyValue;
